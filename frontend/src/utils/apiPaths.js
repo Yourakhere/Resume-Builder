37efad029fff95eebb7fdb83064d80c994321d68
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-09-10 13:03:40 +0530

    Update apiPaths.js
    Api change

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,6 +1,6 @@
 //export const BASE_URL = "http://localhost:8000";
-export const BASE_URL = "https://resume-builder-backendnew.vercel.app";
-//export const BASE_URL = "https://resume-builder-backend-6zq0.onrender.com";
+//export const BASE_URL = "https://resume-builder-backendnew.vercel.app";
+export const BASE_URL = "https://resume-builder-backend-6zq0.onrender.com";
  ///
 export const API_PATHS = {
   AUTH: {
