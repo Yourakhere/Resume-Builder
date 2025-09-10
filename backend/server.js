286cbc157ede39cee7f20cf2f4b3ac6a7e2baa0a
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-09-10 18:36:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-const connectDB = require("./config/db");
 const path = require("path");
-const resumeRoutes = require("./routes/resumeRoutes"); 
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const resumeRoutes = require("./routes/resumeRoutes");
+
 const app = express();
- 
-const authRoutes = require("./routes/authRoutes");  
- 
+
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "*",
@@ -16,22 +16,22 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
- 
+
 connectDB();
- 
+
 app.use(express.json());
- 
+
 app.use("/api/auth", authRoutes);
- 
+app.use("/api/resume", resumeRoutes);
+
 app.use(
   "/uploads",
   express.static(path.join(__dirname, "uploads"), {
-    setHeaders: (res, path) => {
+    setHeaders: (res, _path) => {
       res.set("Access-Control-Allow-Origin", process.env.CLIENT_URL || "*");
     },
   })
 );
-app.use("/api/resume", resumeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
