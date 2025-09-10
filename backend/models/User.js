352e39f13404f457fd9fff2e84ef085908cadccb
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-09-10 17:45:42 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -9,5 +9,5 @@ const UserSchema = new mongoose.Schema({
 {
     timestamps:true,
 }
-);
-module.exports = mongoose.model("user", UserSchema);
+); 
+module.exports = mongoose.model("User", UserSchema);
