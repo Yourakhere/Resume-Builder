516b8de7744bc3fd1f60a20266ce5f96ab1d4774
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-09-20 21:36:53 +0530

    Update uploadMiddleware.js

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -1,22 +1,26 @@
 const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
 
-const storage = multer.diskStorage({
-    destination: (req,file,cb) => {
-        cb(null,'uploads/');
-    },
-    filename: (req,file,cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    },
+ 
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "profile_images",  
+    allowed_formats: ["jpeg", "png", "jpg"],   
+    public_id: (req, file) => `${Date.now()}-${file.originalname}`,  
+  },
 });
-
-const fileFilter = (req,file,cb) => {
-    const allowedTypes = ['image/jpeg','image/png','image/jpg'];
-    if(allowedTypes.includes(file.mimetype)) {
-        cb(null,true);
-    } else {
-        cb(new Error("Only .jpeg, .jpg and .png"), false);
-    }
+ 
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only .jpeg, .jpg and .png formats are allowed"), false);
+  }
 };
-const upload = multer({ storage, fileFilter});
+ 
+const upload = multer({ storage, fileFilter });
 
-module.exports = upload;
+module.exports = upload; 
